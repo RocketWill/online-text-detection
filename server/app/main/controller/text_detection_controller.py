b797ce31d4bcb5692cbd40ed30532070c3a74ae9
Will Cheng <willcheng@noodoe.com>
2020-04-11 22:32:48 +0800

    add path validation

--- server/app/main/controller/text_detection_controller.py
+++ server/app/main/controller/text_detection_controller.py
@@ -21,31 +21,35 @@ class FileUpload(Resource):
         analysis_id = str(args['analysis_id'])
         resp['id'] = analysis_id
 
-        target_folder = os.path.join(file_storage_path, analysis_id)
-        original_file = [f for f in listdir(target_folder) if f.find("original") > -1][0]
-        resp['images']['original'] = url_for('static', filename="upload" + "/" + analysis_id + '/' + original_file)
+        src_path = os.path.join(file_storage_path, analysis_id)
 
-        abs_src_path = os.path.join(file_storage_path, analysis_id)
+        # check src folder exists
+        try:
+            original_file = [f for f in listdir(src_path) if f.find("original") > -1][0]
+            resp['images']['original'] = url_for('static', filename="upload" + "/" + analysis_id + '/' + original_file)
+        except:
+            resp['status'] = 'Failed'
+            resp['message'] = 'Process failed.'
+            return resp, 200
 
         # check if processed.jpg exists
-        files = [f for f in listdir(abs_src_path) if isfile(join(abs_src_path, f))]
+        files = [f for f in listdir(src_path) if isfile(join(src_path, f))]
         if "processed.jpg" in files:
             resp['images']['processed'] = url_for('static',
                                                   filename="upload" + "/" + analysis_id + '/' + "processed.jpg")
-            return resp
+            return resp, 200
 
         # start text detection
         processed_file = text_detection(analysis_id)
         resp['images']['processed'] = url_for('static', filename="upload" + "/" + analysis_id + '/' + processed_file)
 
-        return resp
+        return resp, 200
 
     def options(self):
         return build_cors_prelight_response()
 
 from app.main.model.text_detect import test
 def text_detection(analysis_id):
-    abs_src_path = os.path.join(file_storage_path, analysis_id)
-    print(abs_src_path)
-    test(abs_src_path)
+    src_path = os.path.join(file_storage_path, analysis_id)
+    test(src_path)
     return "processed.jpg"
